da193ef506165c9f48920c73816d6767e92bb8eb
Uriel Radzyminski <uriel@lightit.io>
2020-10-05 11:13:35 -0300

    Format output data in tabbular form

--- src/ReturnMovieInfo.php
+++ src/ReturnMovieInfo.php
@@ -3,8 +3,9 @@
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-/* use Symfony\Component\Console\Helper\Table; */
+use Symfony\Component\Console\Helper\Table;
 use GuzzleHttp\ClientInterface;
 
 class ReturnMovieInfo extends Command{
@@ -22,38 +23,51 @@ class ReturnMovieInfo extends Command{
   {
       $this->setName('show')
            ->setDescription('Returns information of given movie')
-           ->addArgument('movieName', InputArgument::REQUIRED, 'Movie name');
+           ->addArgument('movieName', InputArgument::REQUIRED, 'Movie name')
+           ->addOption('fullPlot', true ,InputOption::VALUE_NONE, 'Return full movie plot');
           
   }
   public function execute(InputInterface $input, OutputInterface $output)
   {
     $movieName = $input->getArgument('movieName');
+    $fullPlot = $input->getOption('fullPlot');
     //get movie information
-    $movieData = $this->getMovieData($movieName);
+    $movieData = $this->getMovieData($movieName, $fullPlot);
 /*     //print movie info
     $this->renderTabbedJson($movieData, $output); */
     $movieData = json_decode($movieData);
     unset($movieData->Ratings);
-
-    foreach ($movieData as $key => $value) {
+    $this->renderTabbedJson($movieData, $output);
+/*     foreach ($movieData as $key => $value) {
       echo $key;
+      echo '    ';
       echo $value;
+      echo "\r\n";
     }
-
+ */
     return 0;
   }
 
-  private function getMovieData($movieName)
+  private function getMovieData($movieName, $fullPlot)
   {
     $requestTarget = 'http://www.omdbapi.com/?apikey=5266aa44&t='. $movieName;
+    if($fullPlot){
+      $requestTarget = $requestTarget . '&plot=full';
+    }
     $movieDataResponse = $this->client->get($requestTarget)->getBody();
     return $movieDataResponse;
   }
 
-/*   private function renderTabbedJson($movieData, $output)
+  private function renderTabbedJson($movieData, $output)
   {
+    $arraydData = [];
+    $i = 0;
+    foreach ($movieData as $key => $value) {
+      $arraydData[$i]=[$key, $value];
+      $i++;
+    }
     $table = new Table($output);
-    $result = json_decode($movieData);
-
-  } */
+    $table->setRows($arraydData)
+          ->render();
+  }
 }
